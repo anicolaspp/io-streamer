8fc2908910bd317c2294cb25e09bb61baddf976e
Nicolas A Perez <anicolaspp@gmail.com>
2018-05-18 15:06:46 -0400

    calling commander

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -10,16 +10,20 @@ object app {
 
   def main(args: Array[String]) {
 
-    val system = ActorSystem("streamer-server")
+    implicit val system = ActorSystem("streamer-server")
 
     val port = args(0).toInt
     val streamerName = args(1)
 
-    val commander = system.actorOf(Commander.props(port, streamerName))
-    commander ! Commander.StartServer
-
+    runCommander(port, streamerName)
+    
     scala.io.StdIn.readLine()
 
     system.terminate()
+  }
+
+  def runCommander(port: Int, name: String)(implicit system: ActorSystem) = Commander.props(port, name) match {
+    case None         =>  println(s"Streamer ${name} not found")
+    case Some(props)  =>  system.actorOf(props) ! Commander.StartServer
   }
 }
--- src/main/scala/com/nico/tcp/Commander.scala
+++ src/main/scala/com/nico/tcp/Commander.scala
@@ -38,20 +38,3 @@ object Commander {
   case class StartServer()
 }
 
-sealed trait StreamerName
-
-object StreamerName {
-  def apply(name: String): StreamerName = selector.get(name) match {
-    case None       =>  throw new Exception("Streamer name not found")
-    case Some(name) =>  name
-  }
-
-  private val selector = Map(
-    "rn" -> RandomNumbers,
-    "rs" -> RandomSales
-    )
-
-  case object RandomNumbers extends StreamerName
-  case object RandomSales extends StreamerName
-}
- 
