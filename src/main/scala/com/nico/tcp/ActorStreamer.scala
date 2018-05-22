4e0076b074c512d28fe6015101c64832c8e3c0cf
Nicolas A Perez <anicolaspp@gmail.com>
2018-05-22 14:31:21 -0400

    LoggingReceive is a little problematic....

--- src/main/scala/com/nico/tcp/ActorStreamer.scala
+++ src/main/scala/com/nico/tcp/ActorStreamer.scala
@@ -2,6 +2,7 @@ package com.nico.tcp
 
 import java.net.InetSocketAddress
 
+import akka.actor.Actor.Receive
 import akka.actor.{ActorContext, ActorRef, Terminated}
 import akka.event.LoggingReceive
 import akka.io.Tcp
@@ -10,7 +11,7 @@ import com.nico.tcp.Predef._
 
 trait ActorStreamer[A] extends DataReceiver with DataSupplier[A] with DataEncoder[A] {
 
-  def customReceiver(remote: InetSocketAddress, connection: ActorRef)(implicit context: ActorContext) = LoggingReceive {
+  def customReceiver(remote: InetSocketAddress, connection: ActorRef)(implicit context: ActorContext): Receive = {
     case Tcp.Received(data)           => startConnection(data)
     case (`__ticks__`, s: ActorRef)   => context.self ! ActorStreamer.Send(encode(get()), s)
     case ActorStreamer.Send(data, to) => to ! Tcp.Write(data)
