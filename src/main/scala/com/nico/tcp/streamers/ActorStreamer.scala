925f4aca2d0c93abd6cdf95acd8dcbb2388262ac
Nicolas A Perez <anicolaspp@gmail.com>
2018-05-22 23:59:43 -0400

    Using LoggingReceive

--- src/main/scala/com/nico/tcp/streamers/ActorStreamer.scala
+++ src/main/scala/com/nico/tcp/streamers/ActorStreamer.scala
@@ -2,15 +2,15 @@ package com.nico.tcp.streamers
 
 import java.net.InetSocketAddress
 
-import akka.actor.Actor.Receive
 import akka.actor.{ActorContext, ActorRef, Terminated}
+import akka.event.LoggingReceive
 import akka.io.Tcp
 import akka.util.ByteString
 import com.nico.tcp.Predef._
 
 trait ActorStreamer[A] extends DataReceiver with DataSupplier[A] with DataEncoder[A] {
 
-  def customReceiver(remote: InetSocketAddress, connection: ActorRef)(implicit context: ActorContext): Receive = {
+  def customReceiver(remote: InetSocketAddress, connection: ActorRef)(implicit context: ActorContext) = LoggingReceive {
     case Tcp.Received(data)           => startConnection(data)
     case (`__ticks__`, s: ActorRef)   => context.self ! ActorStreamer.Send(encode(get()), s)
     case ActorStreamer.Send(data, to) => to ! Tcp.Write(data)
