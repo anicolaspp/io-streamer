c9f89e2242e9f4da3b73b1be6c347b236f5c3cad
Nicolas A Perez <anicolaspp@gmail.com>
2018-05-24 21:57:57 -0400

    Code Formatting

--- src/main/scala/com/nico/tcp/streamers/SalesStreamer.scala
+++ src/main/scala/com/nico/tcp/streamers/SalesStreamer.scala
@@ -4,31 +4,31 @@ import java.net.InetSocketAddress
 
 import akka.actor._
 import akka.util.ByteString
+import com.nico.tcp.streamers.FileStreamer.{FileFragment, FinalFragment, Fragment}
 import com.nico.tcp.streamers.SalesStreamer.{Item, Sale}
 
-import scala.util.Random
+import scala.util.{Random, Try}
 
 class SalesStreamer(remote: InetSocketAddress, connection: ActorRef)
   extends Actor
     with ActorStreamer[Sale] {
-    
-    context.watch(connection)
 
-    override def receive: Receive = customReceiver(remote, connection)
+  context.watch(connection)
 
-    override def get(): Sale = Sale(randomId, Item(randomId, randomPrice), java.time.LocalDate.now)
+  override def receive: Receive = customReceiver(remote, connection)
 
-    override def encode(data: Sale): ByteString = ByteString(s"{sale: {id: ${data.id}, item: {itemId: ${data.item.id}, price: ${data.item.price}}, time: ${data.time.toString}}}\n")
+  override def get(): Sale = Sale(randomId, Item(randomId, randomPrice), java.time.LocalDate.now)
 
-    private def randomId = Random.nextInt(100000).toString
+  override def encode(data: Sale): ByteString = ByteString(s"{sale: {id: ${data.id}, item: {itemId: ${data.item.id}, price: ${data.item.price}}, time: ${data.time.toString}}}\n")
 
-    private def randomPrice = Random.nextDouble() * 100
+  private def randomId = Random.nextInt(100000).toString
+
+  private def randomPrice = Random.nextDouble() * 100
 }
 
 object SalesStreamer {
-    def props(remote: InetSocketAddress, actor: ActorRef): Props = Props(new SalesStreamer(remote, actor))
+  def props(remote: InetSocketAddress, actor: ActorRef): Props = Props(new SalesStreamer(remote, actor))
 
-    case class Sale(id: String, item: Item, time: java.time.LocalDate)
-
-    case class Item(id: String, price: Double)
+  case class Sale(id: String, item: Item, time: java.time.LocalDate)
+  case class Item(id: String, price: Double)
 }
